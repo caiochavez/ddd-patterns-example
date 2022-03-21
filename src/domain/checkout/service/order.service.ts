666e4c4fe7a96b9c9a93bfc1cc552764eeb27c2b
caiochavez <caaiochavez@gmail.com>
2022-03-21 18:59:22 -0300

    dependencie: change uuidv4 to uuid

--- src/domain/checkout/service/order.service.ts
+++ src/domain/checkout/service/order.service.ts
@@ -1,14 +1,14 @@
 import Order from "../entity/order"
 import Customer from "../../customer/entity/customer"
 import OrderItem from "../entity/order_item"
-import { uuid } from "uuidv4"
+import { v4 as uuidv4 } from "uuid"
 
 export default class OrderService {
 
   static placeOrder (customer: Customer, orderItems: OrderItem[]): Order {
     if (orderItems.length === 0) throw new Error("Order must have at least one order item")
 
-    const order = new Order(uuid(), customer.id, orderItems)
+    const order = new Order(uuidv4(), customer.id, orderItems)
     customer.addRewardPoints(order.totalPrice() / 2)
     return order
   }
